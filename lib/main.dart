d4a7df500e0c72b4cf5b13cd7cfabcddb12a2ded
disminder <kometo@gmail.com>
2025-12-06 19:24:12 +0800

    Release version v0.0.22+22

--- lib/main.dart
+++ lib/main.dart
@@ -82,7 +82,7 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
-        title: Text('${widget.title} $_counter |||'),
+        title: Text('${widget.title} $_counter |||222'),
       ),
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
